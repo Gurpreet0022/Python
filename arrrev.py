d5d3e2e19b6f8ce087c20f2e2eb11ab41c66dffb
Gurpreet Kaur <140369686+Gurpreet0022@users.noreply.github.com>
2025-02-01 22:23:12 +0530

    Add files via upload

---
+++ arrrev.py
@@ -0,0 +1,13 @@
+#array reversal - use reversed()
+k=[1,2,3,4,5]
+
+r = reversed(k)
+
+print(list(r))      #it is imp to convert a array to list 
+
+
+
+#sum of all the elements in a array
+arr=[9,6,5,7,1,2]
+total=sum(arr)
+print(total)
